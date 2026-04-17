0c1f080c262c76e9e2ce4de68a63d25dccd7a2e3
CAP-Mantenimiento <hector.aguilar@cap.edu.mx>
2026-04-17 16:43:18 -0600

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "bitacora-cap-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/app.js",
+  "/manifest.webmanifest",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request))
+  );
+});
